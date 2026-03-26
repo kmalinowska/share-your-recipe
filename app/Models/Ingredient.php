2ec8446f0f10ae7e7c8c027740f341cdcb16ae25
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-03-26 17:17:50 +0100

    refactor: update ingredient model relationships

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Ingredient extends Model
 {
@@ -13,4 +14,11 @@ class Ingredient extends Model
     public $timestamps = false;
     protected $fillable = ['name'];
 
+    //relations
+    //ingredient can be in many recipes
+    public function recipes(): BelongsToMany
+    {
+        return $this->belongsToMany(Recipe::class, 'recipe_ingredients')
+            ->withPivot('quantity', 'unit');
+    }
 }
