351f9ff4552f4830361291935cf4d944f5119104
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-03-26 16:50:02 +0100

    refactor: update recipe model relationships

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Recipe extends Model
 {
@@ -17,4 +20,36 @@ class Recipe extends Model
       'time_preparation',
       'image_path'
     ];
+
+    //relations
+    //recipe belongs to user
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+    //recipe belongs to category
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
+    //recipe has many ingredients (by pivot recipe_ingredients)
+    public function ingredients(): BelongsToMany {
+        return $this->belongsToMany(Ingredient::class, 'recipe_ingredients')
+            ->withPivot('quantity', 'unit');
+    }
+    //recipe has many tags (by pivot recipe_tag)
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'recipe_tag');
+    }
+
+    //recipe has many favourites
+    public function favourites(): HasMany
+    {
+        return $this->hasMany(Favourite::class);
+    }
+
+    //recipe has many comments (only main, without answers)
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class)->whereNull('parent_id');
+    }
 }
