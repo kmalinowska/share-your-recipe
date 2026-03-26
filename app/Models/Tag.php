76264eb109364650c4caa42432835763e9dd8675
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-03-26 17:20:06 +0100

    refactor: update tag model relationships

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,12 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Tag extends Model
 {
     use HasFactory, HasUuids;
 
-    public $timestamps = false; // tabela tags nie ma timestamps
+    public $timestamps = false;
 
     protected $fillable = ['name'];
+
+    //relations
+    // Tag can be assigned to multiple recipes
+    public function recipes(): BelongsToMany
+    {
+        return $this->belongsToMany(Recipe::class, 'recipe_tag');
+    }
 
 }
