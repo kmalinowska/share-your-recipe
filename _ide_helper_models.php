8b9f85cf8b842c11f1d1e23eea30c6fdd9367c5e
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-03-27 15:19:54 +0100

    chore: add IDE helper support for Eloquent methods in models

---
+++ _ide_helper_models.php
@@ -0,0 +1,183 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $slug
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Recipe> $recipes
+ * @property-read int|null $recipes_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Category newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Category newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Category query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Category whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Category whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Category whereSlug($value)
+ */
+	class Category extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property string $id
+ * @property string $recipe_id
+ * @property string|null $user_id
+ * @property string|null $guest_name
+ * @property string $content
+ * @property string|null $parent_id
+ * @property string $created_at
+ * @property-read string $author_name
+ * @property-read Comment|null $parent
+ * @property-read \App\Models\Recipe $recipe
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, Comment> $replies
+ * @property-read int|null $replies_count
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereGuestName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereRecipeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereUserId($value)
+ */
+	class Comment extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property string $id
+ * @property string $user_id
+ * @property string $recipe_id
+ * @property string $created_at
+ * @property-read \App\Models\Recipe $recipe
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Favourite newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Favourite newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Favourite query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Favourite whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Favourite whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Favourite whereRecipeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Favourite whereUserId($value)
+ */
+	class Favourite extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property string $id
+ * @property string $name
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Recipe> $recipes
+ * @property-read int|null $recipes_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ingredient newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ingredient newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ingredient query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ingredient whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ingredient whereName($value)
+ */
+	class Ingredient extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property string $id
+ * @property string $title
+ * @property string $slug
+ * @property string|null $preparation
+ * @property int|null $preparation_time
+ * @property string|null $image_path
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $user_id
+ * @property string $category_id
+ * @property-read \App\Models\Category $category
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Comment> $comments
+ * @property-read int|null $comments_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Favourite> $favourites
+ * @property-read int|null $favourites_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Ingredient> $ingredients
+ * @property-read int|null $ingredients_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Tag> $tags
+ * @property-read int|null $tags_count
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe whereImagePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe wherePreparation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe wherePreparationTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Recipe whereUserId($value)
+ */
+	class Recipe extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property string $id
+ * @property string $name
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Recipe> $recipes
+ * @property-read int|null $recipes_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereName($value)
+ */
+	class Tag extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $avatar
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Comment> $comments
+ * @property-read int|null $comments_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Favourite> $favourites
+ * @property-read int|null $favourites_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Recipe> $recipes
+ * @property-read int|null $recipes_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
