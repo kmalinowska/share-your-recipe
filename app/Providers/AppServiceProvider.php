2f49dc933e6787ff42a7b91df0263f6245665afa
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-03-30 13:56:49 +0200

    feat: provide categories data to navigation via AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // use global category for navbar/sidebar
+        View::composer('*', function($view) {
+            $navCategories = Category::orderBy('name')->get();
+            $view->with('navCategories', $navCategories);
+        });
     }
 }
