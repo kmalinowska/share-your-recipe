2ccbd8ef72c8ae47883ff8db48569fa7deb6bfaa
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-04-02 12:35:03 +0200

    style: remove dark mode styles from footer

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer footer-center p-6 bg-base-200 dark:bg-gray-900 text-base-content border-t border-base-300 dark:border-gray-700">
+<footer class="footer footer-center p-6 bg-base-200 text-base-content border-t border-base-300">
     <div>
         <p class="font-bold">Share Your Recipe</p>
         <p>© {{ date('Y') }} - All rights reserved</p>
