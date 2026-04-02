c8db175a35b73c290323b46895a7fb0663ce2a26
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-04-02 17:32:15 +0200

    feat: add global search to navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,13 @@
 <x-app-layout title="Home">
+    {{-- Hero --}}
     <x-home-hero />
+    {{--  Choose Category  --}}
+{{--    <x-category-grid :categories="$categories" />--}}
+    {{-- Latest Recipes  --}}
+{{--    <x-latest-recipes />--}}
+{{--    --}}{{-- Recent Comments   --}}
+{{--    <div>--}}
+{{--        <x-recent-comments :comments="$recentComments"/>--}}
+{{--    </div>--}}
+
 </x-app-layout>
