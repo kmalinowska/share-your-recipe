3bf6b69674bd8494a53213d24a357f6325b5d39d
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-04-02 17:52:01 +0200

    style: restructure home layout with container and grid

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,18 @@
 <x-app-layout title="Home">
     {{-- Hero --}}
     <x-home-hero />
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
     {{--  Choose Category  --}}
 {{--    <x-category-grid :categories="$categories" />--}}
-    {{-- Latest Recipes  --}}
-{{--    <x-latest-recipes />--}}
-{{--    --}}{{-- Recent Comments   --}}
-{{--    <div>--}}
-{{--        <x-recent-comments :comments="$recentComments"/>--}}
-{{--    </div>--}}
+    {{-- Latest Recipes and recent comments  --}}
+{{--        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 mt-12">--}}
+{{--            <div class="lg:col-span-2">--}}
+{{--                <x-latest-recipes :recipes="$latestRecipes" />--}}
+{{--            </div>--}}
+{{--            <aside>--}}
+{{--                <x-recent-comments :comments="$recentComments" />--}}
+{{--            </aside>--}}
+{{--        </div>--}}
+    </div>
 
 </x-app-layout>
