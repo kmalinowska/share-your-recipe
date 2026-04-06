0b985affd5e6b50da17beefa7849ed0be1cbf29c
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-04-06 16:24:09 +0200

    feat: add RecipeFactory and enable HasFactory on Recipe model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Recipe extends Model
 {
-    use HasUuids;
+    use HasUuids, HasFactory;
     protected $fillable = [
       'user_id',
       'category_id',
