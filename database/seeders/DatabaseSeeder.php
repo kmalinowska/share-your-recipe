6d4e620b4f0db36b24beda2a484f34e64f8feb79
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-04-08 14:05:21 +0200

    chore: register TagSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CategorySeeder::class,
             RecipeSeeder::class,
+            TagSeeder::class
         ]);
     }
 }
