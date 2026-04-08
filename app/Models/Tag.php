815e6d6cf5808395070f22164416e0c39eb33895
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-04-08 14:02:37 +0200

    refactor: add slug handling to Tag model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,13 +6,20 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 class Tag extends Model
 {
     use HasFactory, HasUuids;
 
     public $timestamps = false;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'slug'];
+
+    protected static function booted():void {
+        static::creating(function ($tag) {
+           $tag->slug = Str::slug($tag->name);
+        });
+    }
 
     //relations
     // Tag can be assigned to multiple recipes
