b0207781056ba747f221cfe3d20060f1dac9128e
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-04-08 18:43:21 +0200

    feat: implement automatic slug generation in Ingredient model

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -6,13 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Ingredient extends Model
 {
     use HasFactory, HasUuids;
 
     public $timestamps = false;
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'slug'];
+
+    protected static function booted():void{
+        static::creating(function ($ingredient) {
+            $ingredient->slug = Str::slug($ingredient->name);
+        });
+    }
 
     //relations
     //ingredient can be in many recipes
