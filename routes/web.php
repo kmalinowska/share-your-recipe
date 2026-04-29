d9d23f969b9bd9e9b19a9d01e4b300da626912a1
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-04-29 14:03:24 +0200

    refactor: group favourite routes under auth middleware in web.php

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Profile (with auth)
+// Profile & Favourites (with auth)
 Route::middleware('auth')->group(function () {
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Favourites
+    Route::get('/favourites', [FavouriteController::class, 'index'])->name('favourites.index');
+    Route::post('/recipes/{recipe}/favourite', [FavouriteController::class, 'toggle'])->name('favourites.toggle');
 });
 
 // Recipes
@@ -27,13 +32,6 @@ Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.index'
 Route::get('/recipes/{recipe:slug}', [RecipeController::class, 'show'])->name('recipes.show');
 Route::get('/recipes/tags/{tag:slug}', [RecipeController::class, 'tagIndex'])->name('recipes.tag');
 Route::get('/category/{category:slug}', [RecipeController::class, 'indexByCategory'])->name('recipes.category');
-
-// Favourites
-Route::get('favourites', [FavouriteController::class, 'index'])
-    ->middleware('auth')->name('favourites.index');
-Route::post('/recipes/{recipe}/favourite', [FavouriteController::class, 'toggle'])
-    ->middleware('auth')
-    ->name('favourites.toggle');
 
 // Auth routes (login, register, logout)
 require __DIR__.'/auth.php';
