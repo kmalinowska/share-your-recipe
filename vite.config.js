5d4e2f16967b7e7d14c065918211c5db51373c69
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-06-06 01:31:49 +0200

    fix: globSync is not defined

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import { globSync } from 'fast-glob';
 
 export default defineConfig({
     plugins: [
