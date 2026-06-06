de79464d45400c068a98515ec7c3b4d00fe808fd
Klaudia Malinowska <69631074+kmalinowska@users.noreply.github.com>
2026-06-06 02:13:20 +0200

    add continuous delivery pipeline for deploy branch (#26)
    * Configure Deployer and GitHub Actions
    
    * Fix Deployer: dev deps are needed
    
    * Fix Deployer: container cannot access github repo
    
    * Debugging deployer
    
    * Fix Deployer: another try
    
    * fix: vite doesnot process images
    
    * fix: globSync is not defined
    
    * fix: vite doesnot process images: add eager loading

---
+++ deploy.php
@@ -0,0 +1,38 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('application', 'share-your-recipe');
+set('repository', 'git@github.com:kmalinowska/share-your-recipe.git');
+
+set('git_tty', false);
+
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
+add('writable_dirs', ['bootstrap/cache', 'storage']);
+
+// Hosts
+
+host('get.ip.modus.ovh')
+    ->set('remote_user', 'deployer')
+    ->set('port', 22678)
+    ->set('deploy_path', '/var/www/share-your-recipe')
+    ->set('forward_agent', true);
+
+
+// Hooks
+
+task('deploy:upload-assets', function () {
+    upload('public/build', '{{release_path}}/public');
+});
+
+after('deploy:update_code', 'deploy:upload-assets');
+
+task('deploy:php-fpm:reload', function () {
+    run('sudo /usr/bin/systemctl reload php8.5-fpm');
+});
+after('deploy:failed', 'deploy:unlock');
+after('deploy:success', 'deploy:php-fpm:reload');
